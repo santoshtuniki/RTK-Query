29cec3da6fbc168fdd3d619661bb83078d975c41
Saisantoshkumar_Tuniki <saisantosh.tuniki@onpasive.com>
2023-09-13 15:08:42 +0530

    Pokemon skeleton added

---
+++ Pokemon/rtk-tutorial/src/App.tsx
@@ -0,0 +1,14 @@
+// component imports
+
+// css imports
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			APP
+		</div>
+	);
+}
+
+export default App;
---
+++ Pokemon/rtk-tutorial/src/index.tsx
@@ -0,0 +1,18 @@
+// module imports
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// component imports
+import App from './App';
+
+// css imports
+import './index.css';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
